9af063fba79ca590d9cae6629674f56fb53c506b
root <root@TABLET-K7K3VGHE.localdomain>
2025-11-21 16:15:52 -0600

    Enable OpenTelemetry instrumentation and update Dockerfile port

---
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -0,0 +1,63 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents.models import FunctionTool, ToolSet
+from typing import Callable, Set, Any
+from tools.discountLogic import calculate_discount
+# from tools.aiSearchTools import product_data_ai_search
+from dotenv import load_dotenv
+load_dotenv()
+
+# Read prompt file
+CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
+with open(CL_PROMPT_TARGET, 'r', encoding='utf-8') as file:
+    CL_PROMPT = file.read()
+
+#Create project client
+project_endpoint= os.getenv("AZURE_AI_AGENT_ENDPOINT")
+agent_id = os.environ["cora"]
+
+project_client = AIProjectClient(
+    endpoint=project_endpoint,
+    credential=DefaultAzureCredential(),
+)
+
+# Define tools
+user_functions: Set[Callable[..., Any]] = {
+    calculate_discount,
+}
+
+# Initialize agent toolset with user functions
+functions = FunctionTool(user_functions)
+toolset = ToolSet()
+toolset.add(functions)
+project_client.agents.enable_auto_function_calls(tools=functions)
+
+with project_client:
+    agent_exists = False
+    if agent_id:
+        # Check if agent exists.
+        agent = project_client.agents.get_agent(agent_id)
+        print(f"Retrieved existing agent, ID: {agent.id}")
+        agent_exists = True
+    
+    if agent_exists:
+        agent = project_client.agents.update_agent(
+            agent_id=agent.id,
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name="Cora",  # Name of the agent
+            instructions=CORA_PROMPT,  # Updated instructions for the agent
+            # toolset=toolset
+        )
+        print(f"Updated agent, ID: {agent.id}")
+    else:
+        agent = project_client.agents.create_agent(
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name="Cora",  # Name of the agent
+            instructions=CORA_PROMPT,  # Instructions for the agent
+            # toolset=toolset
+        )
+        print(f"Created agent, ID: {agent.id}")
+
